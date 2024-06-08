f922e21dc118b726faffa663567d2d4f02f8fed1
behtash-fani <behtash.fani@gmail.com>
2024-06-08 00:03:54 +0000

    but fix

--- src/database.py
+++ src/database.py
@@ -1,7 +1,4 @@
-# database.py
-
 import sqlite3
-from datetime import datetime
 
 class Database:
     def __init__(self, db_name):
@@ -13,7 +10,8 @@ class Database:
         self.cursor.execute('''
             CREATE TABLE IF NOT EXISTS users (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                telegram_id INTEGER UNIQUE
+                telegram_id INTEGER UNIQUE,
+                username TEXT
             )
         ''')
         self.cursor.execute('''
@@ -22,39 +20,50 @@ class Database:
                 user_id INTEGER,
                 video_id TEXT,
                 title TEXT,
-                date_added TEXT,
                 status TEXT,
+                date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 UNIQUE(user_id, video_id),
                 FOREIGN KEY(user_id) REFERENCES users(id)
             )
         ''')
         self.conn.commit()
 
-    def add_user(self, telegram_id):
-        self.cursor.execute(
-            'INSERT OR IGNORE INTO users (telegram_id) VALUES (?)', (telegram_id,))
+    def add_user(self, telegram_id, username):
+        self.cursor.execute('''
+            INSERT OR REPLACE INTO users (telegram_id, username) 
+            VALUES (?, ?)
+        ''', (telegram_id, username))
         self.conn.commit()
 
-    def add_youtube_link(self, user_id, video_id, title):
-        date_added = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    def user_exists(self, telegram_id):
         self.cursor.execute('''
-            INSERT OR REPLACE INTO youtube_links (user_id, video_id, title, date_added, status)
-            VALUES (
-                ?,
-                ?,
-                ?,
-                ?,
-                'pending'
-            )
-        ''', (user_id, video_id, title, date_added))
+            SELECT 1 FROM users WHERE telegram_id = ?
+        ''', (telegram_id,))
+        return self.cursor.fetchone() is not None
+
+    def add_youtube_link(self, user_id, video_id, title):
+        self.cursor.execute('''
+            INSERT OR REPLACE INTO youtube_links (user_id, video_id, title, status) 
+            VALUES (?, ?, ?, 'pending')
+        ''', (user_id, video_id, title))
         self.conn.commit()
 
     def update_link_status(self, user_id, video_id, status):
-        self.cursor.execute(
-            'UPDATE youtube_links SET status = ? WHERE user_id = ? AND video_id = ?', (status, user_id, video_id))
+        self.cursor.execute('''
+            UPDATE youtube_links 
+            SET status = ? 
+            WHERE user_id = ? AND video_id = ?
+        ''', (status, user_id, video_id))
         self.conn.commit()
 
     def get_user_links(self, user_id):
-        self.cursor.execute(
-            'SELECT video_id, title, status FROM youtube_links WHERE user_id = ?', (user_id,))
+        self.cursor.execute('''
+            SELECT video_id, title, status 
+            FROM youtube_links 
+            WHERE user_id = ? 
+            ORDER BY date_added
+                ''', (user_id,))
         return self.cursor.fetchall()
+
+    def close(self):
+        self.conn.close()
--- src/main.py
+++ src/main.py
@@ -1,8 +1,7 @@
 import os
 import logging
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardMarkup, KeyboardButton
 from dotenv import load_dotenv
 from yt_dl import get_video_details, is_valid_youtube_url, download_video, is_youtube_playlist
 from database import Database
@@ -30,17 +29,20 @@ keyboard.add(KeyboardButton("مشاهده لینک‌های قبلی"))
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     user_id = message.from_user.id
-    db.add_user(user_id)
+    username = message.from_user.username
+    db.add_user(user_id, username)
     await message.answer_sticker("CAACAgIAAxkBAAEMNRRmVHYlX3AeIP2klFDB-7Q_bDzvJwACCgADJHFiGtSUmaRviPBGNQQ")
-    await message.answer("به پاندا دی ال خوش آمدید.", reply_markup=keyboard)
-    await message.answer("در اینجا شما می‌توانید یک لینک یوتیوب ارسال کنید و ما ویدیو را برای شما آپلود و لینک دانلود بدون فیلتر ارائه می‌دهیم.")
-    await message.answer("لطفاً یک لینک یوتیوب ارسال کنید.")
+    await message.answer("سلام، به پاندا دی‌ال خوش آمدید.", reply_markup=keyboard)
+    await message.answer("لطفاً یک لینک یوتیوب ارسال کنید تا ویدیو آن را برای شما بارگذاری کنیم و لینک بدون فیلتر ارائه دهیم.")
 
 @dp.message_handler(lambda message: message.text == "مشاهده لینک‌های قبلی")
 async def show_user_links(message: types.Message):
     user_id = message.from_user.id
-    links = db.get_user_links(user_id)
+    if not db.user_exists(user_id):
+        await message.answer("ابتدا دستور /start را بزنید.")
+        return
 
+    links = db.get_user_links(user_id)
     if not links:
         await message.answer("هیچ لینکی برای شما ثبت نشده است.")
     else:
@@ -48,46 +50,46 @@ async def show_user_links(message: types.Message):
             video_url = f'https://www.youtube.com/watch?v={video_id}'
             await message.answer(
                 f"نام ویدیو: \n`{title}`\nلینک ویدیو: \n`{video_url}`", 
-                parse_mode='Markdown',
+                parse_mode="Markdown", 
                 disable_web_page_preview=True
             )
 
 @dp.message_handler()
 async def get_youtube_link(message: types.Message):
     user_id = message.from_user.id
+    username = message.from_user.username
+    if not db.user_exists(user_id):
+        await message.answer("ابتدا دستور /start را بزنید.")
+        return
+    
     url_is_valid = is_valid_youtube_url(message.text)
     video_url = message.text
-    video_details = get_video_details(video_url)
-    video_id = video_details['video_id']
-    title = video_details['title']
+    
     if url_is_valid:
         if is_youtube_playlist(video_url):
-            await message.answer("در حال حاضر امکان دانلود پلی‌لیست‌های یوتیوب وجود ندارد. لطفاً یک لینک ویدیوی تکی ارسال کنید.")
+            await message.answer("در حال حاضر نمی‌توانیم پلی‌لیست‌های یوتیوب را دانلود کنیم. لطفاً یک لینک ویدیو تکی ارسال کنید.")
         else:
+            video_details = get_video_details(video_url)
+            video_id = video_details['video_id']
+            title = video_details['title']
             db.add_youtube_link(user_id, video_id, title)
-            await message.answer("لینک شما معتبر است.")
-            await message.answer("لطفاً چند لحظه صبر کنید تا اطلاعات ویدیو برای شما ارسال شود.")
-            await message.answer(f"عنوان ویدیو: \n {video_details['title']}")
-            await message.answer("تصویر کاور ویدیو:")
+            await message.answer("لینک معتبر است. لطفاً چند لحظه صبر کنید تا اطلاعات ویدیو به شما نمایش داده شود.")
+            await message.answer(f"عنوان ویدیو:\n {title}")
+            await message.answer("کاور ویدیو:")
             await message.answer_photo(video_details['cover_url'])
 
             # Create an inline keyboard
             keyboard = InlineKeyboardMarkup(row_width=2)
             for fmt in video_details['formats']:
                 if fmt["extension"] == 'mp4' or fmt["extension"] == 'mp3':
-                    if fmt["extension"] == 'mp4':
-                        button_text = f"{fmt['resolution']} - MP4"
-                    elif fmt["extension"] == 'mp3':
-                        button_text = f"{fmt['resolution']} - MP3"
-                    callback_data = f"{video_id}__{fmt['format_id']}__{fmt['resolution']}__{message.from_user.id}"
-                    keyboard.insert(InlineKeyboardButton(
-                        text=button_text, callback_data=callback_data))
+                    button_text = f"{fmt['resolution']} - {fmt['extension'].upper()}"
+                    callback_data = f"{video_id}__{fmt['format_id']}__{fmt['resolution']}__{user_id}"
+                    keyboard.insert(InlineKeyboardButton(text=button_text, callback_data=callback_data))
 
-            await message.answer("لطفاً کیفیت مورد نظر خود را انتخاب کنید:", reply_markup=keyboard)
+            await message.answer("لطفاً کیفیت مورد نظر را انتخاب کنید:", reply_markup=keyboard)
     else:
         await message.answer_sticker("CAACAgIAAxkBAAEMNRlmVHrNarqWDOtaoyqP0Zc4kGc6bQACDAADJHFiGsekexRHa4hiNQQ")
-        await message.answer("لینک ارسال شده نامعتبر است.")
-        await message.answer("لطفاً یک لینک یوتیوب معتبر ارسال کنید.")
+        await message.answer("لینک ارسالی معتبر نیست. لطفاً یک لینک یوتیوب صحیح ارسال کنید.")
 
 @dp.callback_query_handler()
 async def download_video_callback(callback_query: types.CallbackQuery):
@@ -100,34 +102,37 @@ async def download_video_callback(callback_query: types.CallbackQuery):
         resolution = data[2]
         user_id = data[3]
 
+        if not db.user_exists(user_id):
+            await callback_query.message.answer("ابتدا دستور /start را بزنید.")
+            return
+
         # Update the status to pending before starting the download
         db.update_link_status(user_id, video_id, 'pending')
 
         if format_id.startswith('bestaudio'):
-            type = 'audio'
-            await callback_query.message.answer("در حال آماده‌سازی فایل صوتی...")
+            file_type = 'audio'
+            await callback_query.message.answer("فایل صوتی در حال دانلود است.")
         else:
-            type = 'video'
-            await callback_query.message.answer("در حال آماده‌سازی فایل ویدیویی...")
+            file_type = 'video'
+            await callback_query.message.answer("فایل ویدیویی در حال دانلود است.")
 
-        download_result = download_video(
-            video_url, format_id, resolution, user_id, type)
+        download_result = download_video(video_url, format_id, resolution, user_id, file_type)
 
         if download_result['status'] == 'success':
             file_size = bucket.get_object_detail(download_result['file_name'])
             db.update_link_status(user_id, video_id, 'success')
             await callback_query.message.answer(
-                f"فرآیند دانلود با موفقیت انجام شد.\nاندازه فایل: {file_size}\nلینک دانلود: \n{download_result['file_url']}\n توجه داشته باشید که این لینک تا ۱ ساعت فعال است."
+                f"دانلود با موفقیت انجام شد.\nاندازه فایل: {file_size}\nلینک دانلود: {download_result['file_url']}\nاین لینک تا ۱ ساعت معتبر است."
             )
             await callback_query.message.answer("لطفاً ربات ما را به دوستان خود معرفی کنید.\n@pandadl_youtube_bot")
             await callback_query.message.answer_sticker("CAACAgIAAxkBAAEMNSFmVH2EBvyPvxadOMIK7AuPgcIdpgACEQADJHFiGg4fi9EJ5yBPNQQ")
         else:
             db.update_link_status(user_id, video_id, 'fail')
-            await callback_query.message.answer("خطایی در فرآیند دانلود رخ داده است. لطفاً دوباره تلاش کنید.")
+            await callback_query.message.answer("مشکلی در دانلود فایل پیش آمد. لطفاً دوباره تلاش کنید.")
             await callback_query.message.answer_sticker("CAACAgIAAxkBAAEMNSNmVH3HK8IM8ZO0akF2FdirwHnP-wACEAADJHFiGpr6FCbQRHAxNQQ")
     except Exception as e:
         logging.error(f"Unexpected error: {e}")
-        await callback_query.message.answer("مشکلی در پردازش درخواست شما پیش آمده است. لطفاً دوباره تلاش کنید.")
+        await callback_query.message.answer("مشکلی در پردازش درخواست شما پیش آمد. لطفاً دوباره تلاش کنید.")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
