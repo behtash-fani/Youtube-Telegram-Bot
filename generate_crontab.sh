8224bac53a628cd8583af57d2cf174083ae27938
behtash-fani <behtash.fani@gmail.com>
2024-06-08 22:39:13 +0000

    change cronjob timer

--- generate_crontab.sh
+++ generate_crontab.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 # Define the crontab content
-CRONTAB_CONTENT="0 * * * * root python /app/src/bucket_tool.py >> /var/log/cron.log 2>&1
-"
+CRONTAB_CONTENT="*/5 * * * * root python /app/src/bucket_tool.py >> /var/log/cron.log 2>&1"
 
 # Write the crontab content to a file
 echo -e "$CRONTAB_CONTENT" > /etc/cron.d/delete_files_cron
