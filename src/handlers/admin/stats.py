5b228a270648f6494319944c3cad7c1a0184f33e
behtash-fani <behtash.fani@gmail.com>
2025-05-21 20:40:15 +0330

    fix stats for admin folder

--- src/handlers/admin/stats.py
+++ src/handlers/admin/stats.py
@@ -9,6 +9,10 @@ from aiogram.types import FSInputFile
 router = Router()
 db = BotDB()
 
+# مسیر فولدر آمار (خارج از src)
+STATS_FOLDER = ('../stats/')
+os.makedirs(STATS_FOLDER, exist_ok=True)
+
 @router.message(Command(commands=["stats"]))
 async def send_stats_to_admin(message: Message):
     if message.from_user.id not in ADMIN_IDS:
@@ -18,23 +22,30 @@ async def send_stats_to_admin(message: Message):
     users_count = db.get_total_users()
     videos_count = db.get_total_videos()
 
-    # ارسال آمار به ادمین
+    # پیام آماری
     text = (
         f"📊 <b>آمار کلی ربات</b>\n\n"
         f"👥 تعداد کاربران: <b>{users_count}</b>\n"
         f"🎥 تعداد ویدیوهای دانلود شده: <b>{videos_count}</b>"
     )
-
-    # ارسال آمار
     await message.answer(text, parse_mode="HTML")
 
-    # ذخیره داده‌های جدول‌های مختلف به فایل CSV
-    db.save_table_to_csv('users', 'users_data.csv')
-    db.save_table_to_csv('youtube_links', 'youtube_links_data.csv')
+    # مسیر کامل فایل‌ها
+    users_csv_path = os.path.join(STATS_FOLDER, "users_data.csv")
+    links_csv_path = os.path.join(STATS_FOLDER, "youtube_links_data.csv")
 
-    # ارسال فایل‌های CSV به ادمین
+    # ذخیره فایل‌ها
+    db.save_table_to_csv('users', users_csv_path)
+    db.save_table_to_csv('youtube_links', links_csv_path)
+
+    # ارسال فایل‌ها
     await message.answer("✅ آمار دیتابیس با موفقیت ذخیره شد. فایل‌های CSV در اینجا هستند:")
-    users_data_file = FSInputFile("users_data.csv", filename="users_data.csv")
-    await message.answer_document(users_data_file)
-    youtube_links_data_file = FSInputFile("youtube_links_data.csv", filename="youtube_links_data.csv")
-    await message.answer_document(youtube_links_data_file)
+    await message.answer_document(FSInputFile(users_csv_path, filename="users_data.csv"))
+    await message.answer_document(FSInputFile(links_csv_path, filename="youtube_links_data.csv"))
+
+    # حذف فایل‌ها بعد از ارسال
+    # try:
+    #     os.remove(users_csv_path)
+    #     os.remove(links_csv_path)
+    # except Exception as e:
+    #     await message.answer(f"⚠️ خطا در حذف فایل‌ها: {e}")
